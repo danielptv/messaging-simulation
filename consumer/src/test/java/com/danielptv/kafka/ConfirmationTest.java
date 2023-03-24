92f5f4fc746782f5cdf026e8db2d1f737728b915
Daniel Purtov <daniel.purtov@gmail.com>
2023-03-24 07:18:06 +0100

    Improve tests to not use reflection

--- consumer/src/test/java/com/danielptv/kafka/ConfirmationTest.java
+++ consumer/src/test/java/com/danielptv/kafka/ConfirmationTest.java
@@ -1,6 +1,8 @@
 package com.danielptv.kafka;
 
 import com.danielptv.rest.ConfirmationController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -9,12 +11,13 @@ import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.findAll;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,40 +30,38 @@ import static org.junit.jupiter.api.condition.JRE.JAVA_20;
 @SpringBootTest
 @AutoConfigureWireMock(port = 8092)
 @EnabledForJreRange(min = JAVA_19, max = JAVA_20)
+@SuppressWarnings("MagicNumber")
 public class ConfirmationTest {
     private static final String PRODUCER_ENDPOINT = "http://localhost:8092";
-    ConfirmationController controller;
+    private final ConfirmationController controller = new ConfirmationController();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @BeforeAll
     void setup() {
-        controller = new ConfirmationController();
         stubFor(post(urlEqualTo("/"))
                 .willReturn(aResponse()
-                        .withStatus(201)));
+                        .withStatus(HttpStatus.CREATED.value())));
     }
 
     @Test
     @DisplayName("Send order confirmations to producers")
-    void send() throws NoSuchFieldException, IllegalAccessException {
+    void send() throws JsonProcessingException {
         // arrange
-        var order = OrderModel.builder()
+        final var order = OrderModel.builder()
                 .id(UUID.randomUUID())
                 .orderType("testOrderType")
                 .producerEndpoint(PRODUCER_ENDPOINT)
                 .build();
 
         // act
-        controller.send(order);
-        var responsePrivate = ConfirmationController.class.getDeclaredField("response");
-        responsePrivate.setAccessible(true);
-        final var response = (ResponseEntity<?>) responsePrivate.get(controller);
-        var confirmationPrivate = ConfirmationController.class.getDeclaredField("confirmation");
-        confirmationPrivate.setAccessible(true);
-        final var confirmation = (ConfirmationModel) confirmationPrivate.get(controller);
+        final var response = controller.send(order);
+        final var requests = findAll(postRequestedFor(urlEqualTo("/")));
 
         // assert
         assertThat(response).isNotNull();
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(requests).isNotEmpty();
+        final var confirmation = mapper.readValue(requests.get(0).getBodyAsString(), ConfirmationModel.class);
         assertThat(confirmation.orderId()).isEqualTo(order.id());
     }
 }
--- consumer/src/test/java/com/danielptv/kafka/OrderConsumerTest.java
+++ consumer/src/test/java/com/danielptv/kafka/OrderConsumerTest.java
@@ -1,6 +1,8 @@
 package com.danielptv.kafka;
 
-import com.danielptv.rest.ConfirmationController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -10,16 +12,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.TestPropertySource;
 
 import java.util.UUID;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.github.tomakehurst.wiremock.client.WireMock.findAll;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.jupiter.api.condition.JRE.JAVA_19;
 import static org.junit.jupiter.api.condition.JRE.JAVA_20;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,23 +36,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 @EmbeddedKafka(topics = "orders", partitions = 1)
 @TestPropertySource(
         locations = "classpath:application.yml",
-        properties = {
-                "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}",
-                "CONSUMER_TYPE=hardware"
-        })
+        properties = {"spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}", "CONSUMER_TYPE=hardware"})
 @EnabledForJreRange(min = JAVA_19, max = JAVA_20)
 @AutoConfigureWireMock(port = 8092)
+@SuppressWarnings("MagicNumber")
 public class OrderConsumerTest {
     @Autowired
     private KafkaTemplate<String, OrderModel> sender;
-    @Autowired
-    private OrderConsumer consumer;
     @Value("${CONSUMER_TYPE}")
     private String validConsumer;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeAll
+    void setup() {
+        stubFor(post(urlEqualTo("/"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.CREATED.value())));
+    }
 
     @Test
-    @DisplayName("Message consumption with matching order type")
-    public void consumeMessage() throws InterruptedException {
+    @DisplayName("Message consumption with correct order type and confirmation sending")
+    public void consumeAndConfirm() throws InterruptedException, JsonProcessingException {
         // arrange
         final var order = OrderModel
                 .builder()
@@ -61,15 +69,17 @@ public class OrderConsumerTest {
         // act
         sender.send("orders", order);
         Thread.sleep(1000);
+        final var requests = findAll(postRequestedFor(urlEqualTo("/")));
+        final var confirmation = mapper.readValue(requests.get(0).getBodyAsString(), ConfirmationModel.class);
 
         // assert
-        assertThat(consumer.payload).isNotNull();
-        assertEquals(order, consumer.payload);
+        assertThat(confirmation.orderId()).isEqualTo(order.id());
+        assertThat(confirmation.orderType()).isEqualTo(validConsumer);
     }
 
     @Test
-    @DisplayName("Message consumption with wrong producer id")
-    public void dontConsumeMessage() throws InterruptedException {
+    @DisplayName("Message consumption with wrong order type")
+    public void dontConsume() throws InterruptedException {
         // arrange
         final var order = OrderModel
                 .builder()
@@ -82,44 +92,9 @@ public class OrderConsumerTest {
         // act
         sender.send("orders", order);
         Thread.sleep(1000);
+        final var requests = findAll(postRequestedFor(urlEqualTo("/")));
 
         // assert
-        assertThat(consumer.payload).isNull();
-    }
-
-    @Test
-    @DisplayName("Message consumption and confirmation sending")
-    public void consumeAndConfirm() throws NoSuchFieldException, IllegalAccessException, InterruptedException {
-        // arrange
-        stubFor(post(urlEqualTo("/"))
-                .willReturn(aResponse()
-                        .withStatus(201)));
-        final var order = OrderModel
-                .builder()
-                .id(UUID.randomUUID())
-                .orderType(validConsumer)
-                .message("This is a test.")
-                .producerEndpoint("http://localhost:8092")
-                .build();
-
-        // act
-        sender.send("orders", order);
-        Thread.sleep(1000);
-
-        final var controller = consumer.confirmationController;
-        var responsePrivate = ConfirmationController.class.getDeclaredField("response");
-        responsePrivate.setAccessible(true);
-        final var response = (ResponseEntity<?>) responsePrivate.get(controller);
-        var confirmationPrivate = ConfirmationController.class.getDeclaredField("confirmation");
-        confirmationPrivate.setAccessible(true);
-        final var confirmation = (ConfirmationModel) confirmationPrivate.get(controller);
-
-        // assert
-        assertThat(consumer.payload).isNotNull();
-        assertEquals(order, consumer.payload);
-        assertThat(response).isNotNull();
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(confirmation.orderId()).isEqualTo(order.id());
-        assertThat(confirmation.orderType()).isEqualTo(validConsumer);
+        assertThat(requests).isEmpty();
     }
 }
